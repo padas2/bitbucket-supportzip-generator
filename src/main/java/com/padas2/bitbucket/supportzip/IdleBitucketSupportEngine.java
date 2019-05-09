8160038882f654c23a209200e99779f87d6b45b1
padas2 <Supreeth.Padavala@in.pega.com>
2019-05-09 10:05:53 +0530

    Added EmptyBitbucketSupportZip Engine

---
+++ src/main/java/com/padas2/bitbucket/supportzip/IdleBitucketSupportEngine.java
@@ -0,0 +1,7 @@
+package com.padas2.bitbucket.supportzip;
+
+public class IdleBitucketSupportEngine extends BitbucketSupportZipEngine{
+    public IdleBitucketSupportEngine() {
+        super(null);
+    }
+}
